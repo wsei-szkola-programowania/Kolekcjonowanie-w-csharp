942d039c2e998a387c71b8b20316aa69bbed9af3
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-05-28 17:58:02 +0200

    Tablice

---
+++ Kolekcje/Tablice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] tab;
+            tab = new int[10];
+            Random los = new Random();
+            for (var i = 0; i < tab.Length; i++)
+            {
+                tab[i] = los.Next(-10, 11); // od -10 do 10
+            }
+
+            tab[0] = tab[2] = tab[4] = -1;
+            foreach(var x in tab)
+            {
+                Console.WriteLine(x);
+            }
+
+            var napis = string.Join(' ', tab);
+            Console.WriteLine(napis);
+
+            Array.Reverse(tab);
+            Console.WriteLine(string.Join(' ', tab));
+            Array.Sort(tab);
+            Console.WriteLine(string.Join(' ', tab));
+        }
+    }
+}
